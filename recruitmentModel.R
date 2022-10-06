b6b050769e53810e15f580a1f770f02cd1fd90fa
campbellfraser <fraser.campbell@students.plymouth.ac.uk>
2022-10-06 16:19:56 +0200

    Some code to generate graphs for recruitment~lipids

--- recruitmentModel.R
+++ recruitmentModel.R
@@ -35,18 +35,106 @@ for (i in years){
   }
 }
 
-inspectaMonth = readline(prompt="Months: ") %>%
-  strsplit("[[:space:]]") %>%
-  unlist()
+# Graphs plotting lipid content of Jul/Aug/Sep against recruitment of following year ####
+# I HATE how I did all these.... I promise it's just because I didn't have the time to make a lovely loop.....
 
-krillINFO.reduced = dplyr::filter(krillINFO.months,grepl())
+#30mm
+recruitment30m.staggered = recruitmentINFO.years[-c(1),] %>%
+  select(recruitment.30mm) %>% 
+  add_row(recruitment.30mm=NaN)
 
-ggRecruitment = ggplot(krillINFO, aes(x=meanLipids,y=recruitment)) +
+july30 = dplyr::filter(krillINFO.months,grepl("July",mo)) %>% add_column(recruitment30m.staggered)
+august30 = dplyr::filter(krillINFO.months,grepl("August",mo)) %>% add_column(recruitment30m.staggered)
+september30 = dplyr::filter(krillINFO.months,grepl("September",mo)) %>% add_column(recruitment30m.staggered)
+julyMod30 = lm(recruitment.30mm~meanLipids,july30)
+augustMod30 = lm(recruitment.30mm~meanLipids,august30)
+septemberMod30 = lm(recruitment.30mm~meanLipids,september30)
+
+ggJul30 = ggplot(july30, aes(x=meanLipids,y=recruitment.30mm)) +
   geom_point() +
   geom_smooth(method="lm",color="#00BA38") +
-#  stat_regline_equation(label.x = 31, label.y = 1) +
-  geom_text(label=paste("P-val: ",round(lmp(recruitmentMod),digits=4)),
-            x=20,y=0.95) +
-  ggtitle("Recruitment predicted by mean omega-3 content of previous year") +
-  labs(x="Mean omega-3 content (g)",y="Recruitment")
-ggRecruitment + theme_bw()
+  ggtitle("Recruitment (30mm) ~ previous July") +
+  labs(subtitle=paste("P-val: ",round(lmp(julyMod30),digits=4)),
+       x="Mean omega-3 content (g)",y="Recruitment") +
+  xlim(0,25)
+
+ggAug30 = ggplot(august30, aes(x=meanLipids,y=recruitment.30mm)) +
+  geom_point() +
+  geom_smooth(method="lm",color="#00BA38") +
+  ggtitle("Recruitment (30mm) ~ previous August") +
+  labs(subtitle=paste("P-val: ",round(lmp(augustMod30),digits=4)),
+       x="Mean omega-3 content (g)",y="Recruitment") +
+  xlim(0,18)
+
+ggSep30 = ggplot(september30, aes(x=meanLipids,y=recruitment.30mm)) +
+  geom_point() +
+  geom_smooth(method="lm",color="#00BA38") +
+  ggtitle("Recruitment (30mm) ~ previous September") +
+  labs(subtitle=paste("P-val: ",round(lmp(septemberMod30),digits=4)),
+       x="Mean omega-3 content (g)",y="Recruitment") +
+  xlim(0,33)
+
+# 44mm
+recruitment44m.staggered = recruitmentINFO.years[-c(1),] %>%
+  select(recruitment.44mm) %>% 
+  add_row(recruitment.44mm=NaN)
+
+july44 = dplyr::filter(krillINFO.months,grepl("July",mo)) %>% add_column(recruitment44m.staggered)
+august44 = dplyr::filter(krillINFO.months,grepl("August",mo)) %>% add_column(recruitment44m.staggered)
+september44 = dplyr::filter(krillINFO.months,grepl("September",mo)) %>% add_column(recruitment44m.staggered)
+julyMod44 = lm(recruitment.44mm~meanLipids,july44)
+augustMod44 = lm(recruitment.44mm~meanLipids,august44)
+septemberMod44 = lm(recruitment.44mm~meanLipids,september44)
+
+ggJul44 = ggplot(july44, aes(x=meanLipids,y=recruitment.44mm)) +
+  geom_point() +
+  geom_smooth(method="lm",color="#F8766D") +
+  ggtitle("Recruitment (44mm) ~ previous July") +
+  labs(subtitle=paste("P-val: ",round(lmp(julyMod44),digits=4)),
+       x="Mean omega-3 content (g)",y="Recruitment") +
+  xlim(0,25)
+
+ggAug44 = ggplot(august44, aes(x=meanLipids,y=recruitment.44mm)) +
+  geom_point() +
+  geom_smooth(method="lm",color="#F8766D") +
+  ggtitle("Recruitment (44mm) ~ previous August") +
+  labs(subtitle=paste("P-val: ",round(lmp(augustMod44),digits=4)),
+       x="Mean omega-3 content (g)",y="Recruitment") +
+  xlim(0,18)
+
+ggSep44 = ggplot(september44, aes(x=meanLipids,y=recruitment.44mm)) +
+  geom_point() +
+  geom_smooth(method="lm",color="#F8766D") +
+  ggtitle("Recruitment (44mm) ~ previous September") +
+  labs(subtitle=paste("P-val: ",round(lmp(septemberMod44),digits=4)),
+       x="Mean omega-3 content (g)",y="Recruitment") +
+  xlim(0,33)
+
+plot_grid(ncol=3,nrow=2,ggJul30,ggAug30,ggSep30,ggJul44,ggAug44,ggSep44)
+
+# Plot of recruitment vs average lipid content of july, august, and september ####
+JAGlips = read.xlsx("krillINFO_months.xlsx",sheetIndex=2)
+JAGlips$sepLips = as.numeric(JAGlips$sepLips)
+JAGlips$recruitment.44mm = recruitment44m.staggered$recruitment.44mm
+JAGlips$recruitment.30mm = recruitment30m.staggered$recruitment.30mm
+JAGmod_30 = lm(recruitment.30mm~avgLips,JAGlips)
+JAGmod_44 = lm(recruitment.44mm~avgLips,JAGlips)
+# I just realised I have no clue why I put a "G" in JAG. It's meant to stand for "July August September". What's the "G"? "Geptember"? I'm not changing it, I've typed it so many times now. And I refuse to do the simple fix (find and replace), out of principle. What principle? None of your business.
+
+ggJAG_44 = ggplot(JAGlips, aes(x=avgLips,y=recruitment.44mm)) +
+  geom_point() +
+  geom_smooth(method="lm",color="#F8766D") +
+  ggtitle("Recruitment (44mm threshhold) predicted by average lipid content of prior Austral Winter") +
+  labs(subtitle=paste("P-val: ",round(lmp(JAGmod_44),digits=4)),
+       x="Mean omega-3 content",y="Recruitment") +
+  xlim(0,25)
+
+ggJAG_30 = ggplot(JAGlips, aes(x=avgLips,y=recruitment.30mm)) +
+  geom_point() +
+  geom_smooth(method="lm",color="#00BA38") +
+  ggtitle("Recruitment (30mm threshhold) predicted by average lipid content of prior Austral Winter") +
+  labs(subtitle=paste("P-val: ",round(lmp(JAGmod_30),digits=4)),
+       x="Mean omega-3 content",y="Recruitment") +
+  xlim(0,25)
+
+plot_grid(ggJAG_30,ggJAG_44)
